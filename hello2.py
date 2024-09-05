be895b697e64a33741f55adf8c28500bc43ce37b
DC Isenberg <dclem742@gmail.com>
2024-09-05 16:07:54 -0400

    added more exercises

---
+++ hello2.py
@@ -0,0 +1,5 @@
+text = input("What is your name? ")
+print(text.upper())
+
+text = input("What is your name? ")
+print(text[0].upper() + text[1:])
---
+++ work_or_sleep_in.py
@@ -0,0 +1,20 @@
+day = int(input('Day (0-6)?'))
+
+what_to_do = ''
+
+if day == 0:
+    what_to_do = 'sleep in'
+if day == 1:
+    what_to_do = 'go to work'
+if day == 2:
+    what_to_do = 'go to work'
+if day == 3:
+    what_to_do = 'go to work'
+if day == 4:
+    what_to_do = 'go to work'
+if day == 5:
+    what_to_do = 'go to work'
+if day == 6:
+    what_to_do = 'sleep in'
+
+print(what_to_do)
